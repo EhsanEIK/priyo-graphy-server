4fdd31f1830f054869453dc143076a3ba1770bfa
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-08 10:38:11 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+// middleware
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Priyo Graphy Server is running');
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port:", port);
+})
