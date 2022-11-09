de785acdce31ed3daba303994ef1d0ea71ca2e67
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-09 10:33:36 +0600

    added JWT

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
